43536c3745e80c5d1fdb65b9b824833e51331084
akash-deepsharma <akashdeeptechalphonic@gmail.com>
2026-06-19 18:00:32 +0530

    fix: import Link in profile page

--- src/app/account/profile/page.js
+++ src/app/account/profile/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { Camera, Save, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/Button";
